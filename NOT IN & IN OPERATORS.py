9d55804a3a1d34691197b6ca7841612aae12bcee
Gracedonyx <49538027+Gracedonyx@users.noreply.github.com>
2020-09-19 22:37:45 +0100

    Add files via upload

---
+++ NOT IN & IN OPERATORS.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# NOT IN AND IN OPERATORS CHECKS THE FIRST ARGBUMENT TO SEE IF IT IS IN OR NOT IN THE SECOND ARGUMENT
+
+print('p' in 'people')
+
+
+# In[4]:
+
+
+print('park' not in 'peopleparks')
+
+
+# In[ ]:
+
+
+
+
