114c1e14d1768b67868daa15c82b13c14d9ad402
Gracedonyx <49538027+Gracedonyx@users.noreply.github.com>
2020-09-19 22:41:43 +0100

    Add files via upload

---
+++ ACCUMULATOR PATTERN.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+#ACCUMULATOR PATTERN WORKS LIKE GETTING THE TOTAL NUMBER OF THE VARIABLES IN THE LIST
+num = [1, 10, 3, 7, 8, 4, 3, 8, 99, 69]
+accum = 0    # THIS ASSIGNS THE ACCUMULATOR TO BE ZERO 
+for y in num:    #FOR EVERY NUMBER IN num 
+    accum = accum + y    # ADD IT ACCUM AND THEN STORE IN ACCUM
+print(accum)
+
+
+# In[ ]:
+
+
+
+
---
+++ precedence operator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#PREDENCE IS LIKE BODMAS
+#IN PYTHON, MATHEMATICAL EXPRESSION ARE RUN IN A PARTICULAR ORDER
+#PARENTHESIS COMES FIRST FOLLOWED BY EXPONENTIAL, MULIIPLICATION & DIVISION, ADDITION &SUBTRACTION, COMPARISON OPERATOR, NOT OPERATOR AND FINALLY AND&OR OPERATOR.
+
+print ((5**2)-3//2==15)
+#this will first solve the parenthesis then the division before subtraction. then it will compare the answer to 15
+
+
+# In[ ]:
+
+
+
+
